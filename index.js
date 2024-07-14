9d40ef607f361c1a20298ff044331ef2c5fabcbb
Vincent <makokhavomondi@gmail.com>
2024-07-14 16:22:23 +0300

    Move index outside src
    Also slight modifications

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const { TwitterApi } = require('twitter-api-v2')
 const config = require('dotenv').config
 config()
 
+function onConnect(ws){
+    console.log("Client connected on port 8080")
+}
+
+const Server = new WebSocketServer({port: 8080})
+const connection = Rx.Observable.fromEvent(Server, 'connection')
+
 const T = new TwitterApi({
     appKey: process.env.TWITTER_APP_KEY,
     appSecret: process.env.TWITTER_APP_SECRET,
     accessToken: process.env.TWITTER_ACCESS_TOKEN,
     accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
+
+connection.subscribe(onConnect)
